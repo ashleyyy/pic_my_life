24a8bdd56a80e8a68a3de15265d40e8d7c0a5221
inklin <twstacey@gmail.com>
2015-11-19 20:52:38 +0000

    Routes for user signup and signin

--- app/actions.rb
+++ app/actions.rb
@@ -1,8 +1,14 @@
 #HELPERS
 helpers do
+
   def current_user
-   User.find(session[:user_id]) if session[:user_id]
+   @current_user = User.find(session[:user_id]) if session[:user_id]
   end
+
+end
+
+before do
+  current_user
 end
 
 # INDEX PAGE
@@ -22,7 +28,7 @@ get '/photos/new' do
 end
 
 post '/photos' do
-  @photo = current_user.photos.new(params[:photo])
+  @photo = @current_user.photos.new(params[:photo])
   if @photo.save
     redirect '/photos'
   else
@@ -35,7 +41,7 @@ get '/photos/:id' do |id|
   erb :'photos/show'
 end
 
-#USER/SESSIONS
+# SIGN/LOGIN
 get '/signup' do
   @user = User.new
   erb :signup
@@ -45,32 +51,30 @@ get '/signin' do
   erb :signin
 end
 
-
 get '/signout' do
   session.clear
-  redirect '/'
+  redirect '/photos'
 end
 
 post '/signup' do
-  @user = User.new(
-    username: params[:username],
-    password: params[:password],
-    password_confirmation: params[:password_confirmation]
-  )
+  @user = User.new(params[:user])
   if @user.save
-    redirect '/'
+    session[:user_id] = @user.id
+    redirect '/photos'
   else
     erb :'/signup'
   end
 end
 
 post '/signin' do
-  user = User.find_by(username: params[:username], password: params[:password])
-  if user
-    session[:user_id] = user.id
-    redirect '/'
+  @user = User.find_by(params[:user])
+  if @user
+    session[:user_id] = @user.id
+    redirect '/photos'
   else
-    redirect '/signin'
+    @signin_name = params[:user][:username]
+    @error = "Incorrect username or password."
+    erb :'/signin'
   end
 end
 
