ff0d2cd5864f35d0d423d90f564b954bace01837
vagrant <vagrant@lighthouse-labs>
2015-11-19 02:10:22 +0000

    Created the basic website less some features and less design so we can test the back-end

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,10 +5,10 @@ class Photo < ActiveRecord::Base
 
   validates :user_id, presence: true, numericality: true
   validates :url, presence: true
-  validate :url_must_be_valid
+  # validate :url_must_be_valid
 
-  def url_must_be_valid
-    errors.add(:url, "The URL must link to a picture(jpg, jpeg, gif or png)") unless url =~ /(http(s?):)|([/|.|\w|\s])*.(?:jpg|jpeg|gif|png)/
-  end
+  # def url_must_be_valid
+  #   errors.add(:url, "The URL must link to a picture(jpg, jpeg, gif or png)") unless url =~ /(http(s?):)|([/|.|\w|\s])*.(?:jpg|jpeg|gif|png)/
+  # end
 
 end
