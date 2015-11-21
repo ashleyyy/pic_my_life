e9826a62ab092fd1fac6752993246164a16545d6
fabienalbi <fabien.albi@icloud.com>
2015-11-21 18:35:00 +0000

    Fixed redirection on random picture vote

--- app/actions/vote.rb
+++ app/actions/vote.rb
@@ -4,5 +4,9 @@ post '/photos/:id/votes' do |id|
     user_id: @current_user.id,
     context: params[:choice]
     )
-  redirect back
+  if back == "http://localhost:3001/photos/random"
+    redirect "photos/#{photo.id}"
+  else
+    redirect back
+  end
 end
