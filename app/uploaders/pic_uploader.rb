484e611b15b89c5f1cd92a5aa0e363fb91c9fdf4
fabienalbi <fabien.albi@icloud.com>
2015-11-22 01:39:32 +0000

    remove minimagick

--- app/uploaders/pic_uploader.rb
+++ app/uploaders/pic_uploader.rb
@@ -1,11 +1,8 @@
 class PicUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
   storage :file
 
   def store_dir
     'uploads/pics'
   end
-
-  process resize_to_fit: [800, 800]
 
 end
