c5425d8090648e955bb27e7f031aecb27e1af92b
ashleyyy <ashleyfisher@gmail.com>
2015-12-04 23:25:04 +0000

    modified gemfile.lock

--- app_config.rb
+++ app_config.rb
@@ -7,20 +7,19 @@ require 'active_record'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 
-require 'factory_girl'
-require 'faker'
-require 'database_cleaner'
+# require 'factory_girl'
+# require 'faker'
+# require 'database_cleaner'
 
 require './app/models/user'
 require './app/models/photo'
 require './app/models/vote'
 
-require './factories/photos_factory'
-require './factories/users_factory'
-require './factories/votes_factory'
+# require './factories/photos_factory'
+# require './factories/users_factory'
+# require './factories/votes_factory'
 
 require './lib/importer'
-
 
 module AppConfig
 
